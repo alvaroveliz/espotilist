d5a89415553f99bfe0f72690b72712fb976dcc20
Álvaro Véliz <alvaro.veliz@gmail.com>
2018-10-13 02:37:05 -0300

    fix ql

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,38 +5,36 @@
 </template>
 
 <script>
-const stateKey = 'spotify_auth_state';
+const stateKey = 'spotify_auth_state'
 
 export default {
   methods: {
-    generateRandomString: function(length) {
-      var text = '';
-      var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    generateRandomString: function (length) {
+      var text = ''
+      var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
       for (var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
       }
-      return text;
+      return text
     },
     authenticate: function (event) {
-      var client_id = '103b173a2c564c4591fc8af650eff62e'; // Your client id
-      var redirect_uri = 'http://50.116.17.227:3000/'; // Your redirect uri
+      var clientId = '103b173a2c564c4591fc8af650eff62e' // Your client id
+      var redirectUri = 'http://50.116.17.227:3000/' // Your redirect uri
 
-      var state = this.generateRandomString(16);
+      var state = this.generateRandomString(16)
 
-      sessionStorage.setItem(stateKey, state);
-      var scope = 'user-read-private user-read-email';
+      sessionStorage.setItem(stateKey, state)
+      var scope = 'user-library-modify user-modify-playback-state streaming user-read-email user-read-private playlist-modify-private playlist-modify-public'
 
-      var url = 'https://accounts.spotify.com/authorize';
-      url += '?response_type=token';
-      url += '&client_id=' + encodeURIComponent(client_id);
-      url += '&scope=' + encodeURIComponent(scope);
-      url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-      url += '&state=' + encodeURIComponent(state);
+      var url = 'https://accounts.spotify.com/authorize'
+      url += '?response_type=token'
+      url += '&client_id=' + encodeURIComponent(clientId)
+      url += '&scope=' + encodeURIComponent(scope)
+      url += '&redirect_uri=' + encodeURIComponent(redirectUri)
+      url += '&state=' + encodeURIComponent(state)
 
-      console.log(url);
-
-      window.location = url;
+      window.location = url
     }
   }
 }
